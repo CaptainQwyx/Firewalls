878369aabf30e2e461b7f2801be2a3387f03f424
Jeff Anderson <captainqwyx@gmail.com>
2020-04-30 20:12:36 -0500

    winning a level

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -7,9 +7,24 @@ public class Block : MonoBehaviour
     // config params
     [SerializeField] AudioClip breakSound = null;
 
+    // Cached references
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.IncrementBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);        
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
+        level.BlockBroken();
     }
 }
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] int breakableBlocks = 0;  // Serialized for debugging purposes
+
+    // Cached references
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void IncrementBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void BlockBroken()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+}
