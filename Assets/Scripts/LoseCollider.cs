37d2cfaa38233105b6d3f61b3920a0368ea34b13
Jeff Anderson <captainqwyx@gmail.com>
2020-05-04 07:45:23 -0500

    levels 1-3

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -8,7 +8,7 @@ public class LoseCollider : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene("Game Over");
+        SceneManager.LoadScene("z. Game Over");
     }
 
 }
